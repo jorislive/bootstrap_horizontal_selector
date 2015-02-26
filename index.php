4cce2bda3c24725705d623c3a17d6f67b0269308
Joris Wenting <jorislive@hotmail.com>
2015-02-26 21:04:56 +0100

    Initial Commit v0.4

---
+++ index.php
@@ -0,0 +1,115 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Horizontal_Selector Test</title>
+	
+	<!-- Adobe Typekit -->
+	<script src="//use.typekit.net/idq7tbo.js"></script>
+	<script>try{Typekit.load();}catch(e){}</script>
+	
+	<!-- Jquery -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+	
+	<!-- Jquery UI-->
+	<link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/themes/smoothness/jquery-ui.css" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.11.3/jquery-ui.min.js"></script>
+	
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+	
+	<!-- Horizontal Slider -->
+	<link rel="stylesheet" type="text/css" href="css/horizontal_selector.css">
+	<script src="js/horizontal_selector.js"></script>
+</head>
+<body>
+
+<div id="headerTop">
+<table>
+<tr>
+	<td id="logoAccow">&nbsp;</td>
+	<td id="headerTopTitle" class=".tk-expo-sans-pro">
+	<p class="colorBlue">&LT;script&GT;</p>
+	<p>&nbsp;&nbsp;&nbsp;&nbsp;$("select").horizontalSelector();</p>
+	<p class="colorBlue">&LT;/script&GT;</p>
+	
+	
+	</td>
+	<td id="headerTopRight"></td>
+</tr>
+</table>
+<div id="contentSpacer"
+
+
+</div>
+
+<div id="selectorContainer" data-hole="<?php echo $hole_selected ?>">
+
+<form id="testForm">
+
+<select id="testSelectorA" name="testSelector">
+  <option value="0">Volvo</option>
+  <option value="1">Saab</option>
+  <option value="2" >Mercedes</option>
+  <option value="3">Audi</option>
+  <option value="4" >Masserati</option>
+  <option value="5" selected>Renault</option>
+  <option value="6" >Citroën</option>
+  <option value="7">Ferrari</option>
+  <option value="8">Volkswagen</option>  
+</select>
+
+<select id="testSelectorD" name="testSelector4">
+  <option value="0" >Never</option>
+  <option value="1" >Almost Never</option>
+  <option value="2" >Sometimes</option>
+  <option value="3" >Often</option>
+  <option value="4" >Always</option>
+</select>
+
+<select id="testSelectorE" name="testSelector4">
+  <option value="0" >One</option>
+  <option value="1" selected>Two</option>
+  <option value="2" >Three</option>
+</select>
+
+<select id="testSelectorF" name="testSelector4">
+  <option value="0" >Yes</option>
+  <option value="1" >No</option>
+</select>
+
+
+
+</form>
+
+<div id="buttonContainer">
+<button id="submit" class="btn btn-primary-sm">Submit</button>
+</div>
+
+<div id="sizeMe">&nbsp;</div>
+
+</div>
+
+
+<script>
+
+$("select").horizontalSelector();
+
+$("#selectorContainer").resizable({
+	  // Quick and dirty solution...
+      minWidth: 682,
+	  minHeight: 900
+      //handles: 'e, w'
+});
+
+$("#submit").click(function(event){
+	event.preventDefault();
+	var test = $('form').serialize();
+	alert(test);
+});
+
+</script>
+
+</body>
+</html>
---
+++ js/horizontal_selector.js
@@ -0,0 +1,133 @@
+// Functionality
+// This Jquery Extension lets you transform a standard html <select> element 
+// from standard dropdown to horizontal 'dot_below_selection_caption'.
+//  
+//	e.g.
+//	Select 1      Select 2		Select 3 
+//                   ⌂
+//  usage:
+//  $('#someSelectorElement').horizontalSelector();
+//
+//  License: MIT
+//  Written by: Joris Wenting 2015
+
+jQuery.fn.extend({
+	
+	horizontalSelector: function (){
+		$(this).each(function(){
+			$(this).buildSelector();
+		});
+	}
+});
+
+jQuery.fn.extend({
+	
+	buildSelector: function (){
+		
+		//$(this).hide();
+		
+		var selectorId = $(this).attr("id");
+		var hashtagSelectorId = "#" + selectorId;
+		$(hashtagSelectorId).addClass("hs-binded");
+		var optionValueArray = optionValues($(this));
+		var optionHtmlArray = optionHtmls($(this));
+		var numberOfOptions = $(this).children().size();
+		var parent = $(this).parent();
+		var currentSelected = $(this).val();
+		construct();
+		setSelected(currentSelected);
+		setSelected(currentSelected, selectorId)
+			
+		// Events
+		$(function(){
+
+			$("td.hole").click(function(){
+				var destinationId = $(this).attr("selector-id");
+				var previousSelected = "td.selected-hole[selector-id='" + destinationId + "']";
+				$(previousSelected).removeClass("selected-hole");			
+				$(this).addClass("selected-hole");
+				var newValue = $(this).attr("option-id");
+				originalSelect = "#" + destinationId;
+				$(originalSelect).val(newValue);
+			});
+			
+			$(".hs-binded").change(function(){	
+				var destinationId = $(this).attr("id");		
+				var previousSelected = "td.selected-hole[selector-id='" + destinationId + "']";
+				$(previousSelected).removeClass("selected-hole");
+				var newValue = $(this).val();
+				var selectHole = "td[option-id='" + newValue + "'][selector-id='" + destinationId + "']";
+				$(selectHole).addClass("selected-hole");
+			});
+			
+		});		
+	
+		function construct(){
+							
+				// Create HTML for the new selector
+				var newHtml = "<table><tr>";
+				
+				for(i=0; i < numberOfOptions; i++){
+					
+					var value = optionHtmlArray[i];
+					newHtml += "<th class='fixedWidth'><h6>"+ value +"</h6></th>";
+					newHtml += "<th class='sliderSpacerHeading'>&nbsp;</th>";
+				
+				}
+				
+				newHtml += "</tr>";
+				
+				newHtml += "<tr id='selectorOptions'>";
+				
+				var x = 0
+				
+				newHtml += "<td option-id='"+ optionValueArray[x] + "' selector-id='" + selectorId + "' class='hole first-hole'></td>";
+				newHtml += "<td class='hole-spacer'></td>";
+				
+				x++;
+				
+				for(i=1; i < numberOfOptions - 1; i++){
+								
+						newHtml += "<td option-id='" + optionValueArray[i] + "' selector-id='" + selectorId + "' class='hole between-hole'></td>";
+						newHtml += "<td class='hole-spacer'></td>";	
+						x++;
+				}
+				
+				newHtml += "<td option-id='"+ optionValueArray[x] + "' selector-id='" + selectorId + "' class='hole last-hole'></td>";
+				
+				newHtml += "</tr>";
+				
+				newHtml += "</table>";
+				
+				parent.append(newHtml);			
+		};		
+		
+		function optionValues(element){
+			var array = [];
+			element.children().each(function(){
+				// The $(this) selector below refers to the child element !!
+				array.push($(this).attr("value"));	
+			});
+			return array;
+		};
+		
+		function optionHtmls(element){
+			var array = [];
+			element.children().each(function(){
+				// The $(this) selector below refers to the child element !!
+				array.push($(this).html());	
+			});
+			return array;
+		};
+		
+		function setSelected(selectedValue, destinationId){
+			var selectHole = "td[option-id='" + selectedValue + "'][selector-id='" + destinationId + "']";
+			$(selectHole).addClass("selected-hole");
+		}	
+	}
+});
+
+
+		
+
+		
